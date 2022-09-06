ff176e38540907c464dc1b4bc1594fa9a0a58a80
livx5199 <livx5199@stud.kea.dk>
2022-09-06 12:17:44 +0200

    showRgb

--- index.js
+++ index.js
@@ -14,10 +14,28 @@ function updateColor(event) {
     // console.log(colordisplay.style.background);
 
     showHex(input.value);
+    showRgb(input.value);
+    
 }
 
 function showHex(input) {
     console.log(input);
     let hex = document.querySelector(".hex");
     hex.textContent = `HEX: ${input}`;
+
+}
+
+function showRgb(input) {
+    let rgb = document.querySelector(".rgb");
+
+    const rgbCleaned = input.replace("#", "");
+    let hexString = rgbCleaned.match(/.{1,2}/g);  
+
+    let rgbString = [
+        parseInt(hexString[0], 16),
+        parseInt(hexString[1], 16),
+        parseInt(hexString[2], 16)
+    ];
+
+    rgb.textContent = `RGB: ${rgbString}`;
 }
